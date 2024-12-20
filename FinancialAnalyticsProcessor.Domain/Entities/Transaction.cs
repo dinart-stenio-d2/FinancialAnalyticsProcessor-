fd037b69b3bd51b5f8339bffb10a0c0027ecc814
dinart-stenio-d2 <124164706+dinart-stenio-d2@users.noreply.github.com>
2024-12-19 21:26:57 -0300

    created the base architecture of the project

---
+++ FinancialAnalyticsProcessor.Domain/Entities/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using FinancialAnalyticsProcessor.Core.Domain.DomainObjects;
+
+namespace FinancialAnalyticsProcessor.Domain.Entities
+{
+    public class Transaction : Entity, IAggregateRoot  
+    {
+        public Guid TransactionId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Merchant { get; set; }
+
+    }
+}
