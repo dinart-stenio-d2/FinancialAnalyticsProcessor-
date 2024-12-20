d217db807c7ae3a4275db1173cbf825d29211536
dinart-stenio-d2 <124164706+dinart-stenio-d2@users.noreply.github.com>
2024-12-19 22:54:44 -0300

    created infrastructure service to load CSV file (#2)

---
+++ FinancialAnalyticsProcessor.Infrastructure/Services/CsvTransactionLoader.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using FinancialAnalyticsProcessor.Domain;
+using FinancialAnalyticsProcessor.Domain.Entities;
+using System.Globalization;
+
+namespace FinancialAnalyticsProcessor.Infrastructure.Services
+{
+    public class CsvTransactionLoader : ICsvTransactionLoader
+    {
+        public async Task<IEnumerable<Transaction>> LoadTransactionsAsync(Stream csvStream)
+        {
+            using var reader = new StreamReader(csvStream);
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                Delimiter = ","
+            });
+
+            // A leitura de CSV deve ser feita de forma assíncrona
+            var records = await Task.Run(() => csv.GetRecords<Transaction>().ToList());
+            return records;
+        }
+    }
+}
