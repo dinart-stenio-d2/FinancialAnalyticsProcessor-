4b9a1e170c2dee49bef2d5786b0a5c2050d88d18
dinart-stenio-d2 <124164706+dinart-stenio-d2@users.noreply.github.com>
2024-12-19 23:43:30 -0300

    created transaction processor application service (#4)

---
+++ FinancialAnalyticsProcessor.Application/Services/TransactionProcessor.cs
@@ -0,0 +1,45 @@
+﻿using FinancialAnalyticsProcessor.Domain.Entities;
+using FinancialAnalyticsProcessor.Domain.Interfaces.ApplicationServices;
+using FinancialAnalyticsProcessor.Domain.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace FinancialAnalyticsProcessor.Application.Services
+{
+    public class TransactionProcessor : ITransactionProcessor
+    {
+        private readonly IRepository<Transaction> _repository;
+        private readonly ILogger<TransactionProcessor> _logger;
+
+        public TransactionProcessor(IRepository<Transaction> repository, ILogger<TransactionProcessor> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public async Task ProcessTransactionsAsync(IEnumerable<Transaction> transactions)
+        {
+            _logger.LogInformation("Starting transaction processing...");
+
+            if (transactions == null || !transactions.Any())
+            {
+                _logger.LogWarning("No transactions provided for processing.");
+                throw new ArgumentException("No transactions to process.");
+            }
+
+            try
+            {
+                var transactionCount = transactions.Count();
+                _logger.LogInformation("Processing {TransactionCount} transactions.", transactionCount);
+
+                await _repository.BulkInsertAsync(transactions);
+
+                _logger.LogInformation("Successfully processed {TransactionCount} transactions.", transactionCount);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing transactions.");
+                throw; // Re-throw the exception to propagate it up the call stack
+            }
+        }
+    }
+}
