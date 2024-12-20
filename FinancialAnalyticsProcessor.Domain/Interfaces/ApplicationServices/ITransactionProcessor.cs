7bc792e673b195eb6f197ad48d064b87df9af342
dinart-stenio-d2 <124164706+dinart-stenio-d2@users.noreply.github.com>
2024-12-20 00:02:03 -0300

    updated the apllication service (#5)
    * created transaction processor application service
    
    * updated the apllication service

--- FinancialAnalyticsProcessor.Domain/Interfaces/ApplicationServices/ITransactionProcessor.cs
+++ FinancialAnalyticsProcessor.Domain/Interfaces/ApplicationServices/ITransactionProcessor.cs
@@ -5,5 +5,8 @@ namespace FinancialAnalyticsProcessor.Domain.Interfaces.ApplicationServices
     public interface ITransactionProcessor
     {
         Task ProcessTransactionsAsync(IEnumerable<Transaction> transactions);
+        Task<dynamic> PerformAnalysisAsync();
+        Task SaveReportAsync(dynamic report, string outputPath);
+
     }
 }
